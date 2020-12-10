9aa404cf878053ad85b4f2043824459ae0014c33
Ian <ian@weestoater.com>
2020-12-10 20:56:03 +0000

    Set up Car and h2-db; add dummy data;

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,13 +1,36 @@
 package com.packt.cardatabase;
 
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class CardatabaseApplication {
+	@Autowired
+	private CarRepository carRepository;
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CardatabaseApplication.class, args);
+		System.out.println("Hello World!");
 	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			//	Place your code here...
+			//	Save demo data to database
+			carRepository.save(new Car("Ford","Focus","Silver", "SD10ODT", 2010,8600 ));
+			carRepository.save(new Car("Volkswagen", "Golf Mark IV", "Black","SH51OTX", 2001, 2000));
+			carRepository.save(new Car("Honda", "Civic 1.3", "Silver", "SD61AVJ", 2011, 9000));
+			carRepository.save(new Car("Subaru", "XV", "Silver", "SA14CZU", 2014, 11500));
+		};
+	}
+
 
 }
---
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -0,0 +1,78 @@
+package com.packt.cardatabase.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String brand;
+    private String model;
+    private String colour;
+    private String registration;
+    private int year;
+    private int price;
+
+    public Car() {}
+
+    public Car(String brand, String model, String colour, String registration, int year, int price) {
+        this.brand = brand;
+        this.model = model;
+        this.colour = colour;
+        this.registration = registration;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
