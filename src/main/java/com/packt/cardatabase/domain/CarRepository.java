0a396482b10413b03729c753875e894a1d60ca8c
Ian <ian@weestoater.com>
2020-12-10 22:30:13 +0000

    Set up Car and h2-db; add dummy data;

--- src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -1,7 +1,21 @@
 package com.packt.cardatabase.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CarRepository extends CrudRepository <Car, Long> {
+
+    //  Fetch cars by brand
+    @Query("select c from Car where c.brand=?1")
+    List<Car> findByBrand(String brand);
+
+    //  Fetch cars by brand and model
+    @Query("select c from Car where c.brand=?1")
+    List<Car> findByBrandAndModel(String brand, String model);
+
+    //  Fetch cares by brand and colour
+    List<Car> findByBrandAndColour(String brand, String colour);
 
 }
